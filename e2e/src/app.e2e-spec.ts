54e27183522c9dfebb6b687add236ed8ca90cae8
abdallamaher <abdallamaher89@gmail.com>
2022-03-13 17:39:35 +0200

    update angular json name

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe("workspace-project App", () => {
 
   it("should display welcome message", () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual("Welcome to orange-labs!");
+    expect(page.getTitleText()).toEqual("Welcome to kayak!");
   });
 
   afterEach(async () => {
